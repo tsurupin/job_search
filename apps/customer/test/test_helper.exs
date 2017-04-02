4bbfac480eb05c3bb35dc6dc88ff0f6e0f1eb020
Tomoaki Tsuruta <england0112@gmail.com>
2017-04-02 15:49:32 -0700

    Update mix

--- apps/customer/test/test_helper.exs
+++ apps/customer/test/test_helper.exs
@@ -1,5 +1,4 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
-{:ok, _} = Application.ensure_all_started(:wallaby)
 ExUnit.start
 Pact.put(:google_strategy, FakeGoogleStrategy)
 Ecto.Adapters.SQL.Sandbox.mode(Customer.Repo, :manual)
