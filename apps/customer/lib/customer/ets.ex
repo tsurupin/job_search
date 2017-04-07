d38e352d9b626cb831614cf8b26d172b5def3c34
Tomoaki Tsuruta <england0112@gmail.com>
2017-04-07 14:34:04 +0900

    Use global name for ets server

--- apps/customer/lib/customer/ets.ex
+++ apps/customer/lib/customer/ets.ex
@@ -1,32 +1,33 @@
 defmodule Customer.Ets do
    use GenServer
 
-   @name :customer_ets
+   @ets_name :customer_ets
+   @server_name {:global, @ets_name}
 
    # TODO: Need to use global name for distribution
    def start_link() do
-      GenServer.start_link(__MODULE__, {:ok, []}, name: @name)
+      GenServer.start_link(__MODULE__, {:ok, []}, name: @server_name)
    end
 
    def init({:ok, _args}) do
-     ets = :ets.new(@name, [:named_table, :public])
+     ets = :ets.new(@ets_name, [:named_table, :public])
      {:ok, ets}
    end
 
    def reset() do
-     GenServer.call(@name, {:reset})
+     GenServer.call(@server_name, {:reset})
    end
 
    def upsert(%{key: key, value: value} = cache) do
-     GenServer.call(@name, {:upsert, cache})
+     GenServer.call(@server_name, {:upsert, cache})
    end
 
    def async_upsert(%{key: key, value: value} = cache) do
-     GenServer.cast(@name, {:upsert, cache})
+     GenServer.cast(@server_name, {:upsert, cache})
    end
 
    def fetch(key) do
-     GenServer.call(@name, {:fetch, key})
+     GenServer.call(@server_name, {:fetch, key})
    end
 
    def handle_call({:fetch, key}, _from, state) do
@@ -35,24 +36,24 @@ defmodule Customer.Ets do
    end
 
    def handle_call({:upsert, %{key: key, value: value}}, _from, state) do
-     :ets.insert(@name, {key, value})
+     :ets.insert(@ets_name, {key, value})
      value = fetch_value(key)
      {:reply, value, state}
    end
 
-   def handle_call({:reset}, _from, _state) do
-     :ets.delete(@name)
-     ets = :ets.new(@name, [:named_table, :public])
+   def handle_call({:reset, key}) do
+     :ets.delete(@ets_name)
+     ets = :ets.new(@ets_name, [:named_table, :public])
      {:reply, :ok, ets}
    end
 
    def handle_cast({:upsert, %{key: key, value: value}}, state) do
-      :ets.insert(@name, {key, value})
+      :ets.insert(@ets_name, {key, value})
       {:noreply, state}
    end
 
    defp fetch_value(key) do
-     case :ets.lookup(@name, key) do
+     case :ets.lookup(@ets_name, key) do
         [{key, value}] -> {:ok, value}
         [] -> {:error, "not found"}
       end
